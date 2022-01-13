eab97a5d0505d281d62a182921c7ffebef8cef19
logicgaurav <logicgaurav@gmail.com>
2022-01-13 15:39:29 +0530

    Pubmatic Sample

--- android/app/src/main/java/com/example/flutter_native_android_work/NativeView.java
+++ android/app/src/main/java/com/example/flutter_native_android_work/NativeView.java
@@ -1,32 +1,16 @@
 package com.example.flutter_native_android_work;
 
-import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
-import com.example.flutter_native_android_work.R;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
-import com.npaw.youbora.lib6.exoplayer2.Exoplayer2Adapter;
-import com.npaw.youbora.lib6.plugin.Options;
-import com.npaw.youbora.lib6.plugin.Plugin;
-//import com.pubmatic.sdk.common.POBError;
-//import com.pubmatic.sdk.openwrap.banner.POBBannerView;
+import com.pubmatic.sdk.common.POBError;
+import com.pubmatic.sdk.openwrap.banner.POBBannerView;
 import com.pubmatic.sdk.openwrap.eventhandler.dfp.DFPBannerEventHandler;
 
 import java.util.Map;
@@ -35,11 +19,6 @@ import io.flutter.plugin.platform.PlatformView;
 
 class NativeView implements PlatformView {
     private final LinearLayout layout;
-    LinearLayout lin_lay;
-    // private SimpleExoPlayer player;
-    // private DefaultTrackSelector trackSelector;
-    SimpleExoPlayer absPlayerInternal;
-    // PlayerView pvMain;
 
     NativeView(Context context, int id, Map<String, Object> creationParams) {
         layout = new LinearLayout(context);
@@ -47,73 +26,106 @@ class NativeView implements PlatformView {
         layout.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
         // layout.setOrientation(FrameLayout.VERTICAL);
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View v = inflater.inflate(R.layout.activity_mainn, null);
-        PlayerView playerView = v.findViewById(R.id.player_view);
-        lin_lay = v.findViewById(R.id.lin_lay);
+        getPubmatics(context);
+        
+    }
 
-        lin_lay.setBackgroundColor(context.getResources().getColor(R.color.browser_actions_divider_color));
+    private void getPubmatics(Context context) {
+        AdSize customAdSize = new AdSize(300, 250);
 
-        String CONTENT_URL = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
+        DFPBannerEventHandler eventHandler = new DFPBannerEventHandler(context,
+                "/11440465/Zeebiz_AOS/Zeebiz_AOS_HP_ATF_300x250", customAdSize);
 
-        int appNameStringRes = R.string.app_name;
+        POBBannerView banner = new POBBannerView(context,
+                "158141",
+                5777,
+                "/11440465/Zeebiz_AOS/Zeebiz_AOS_HP_ATF_300x250",
+                eventHandler);
 
-        // pvMain = findViewById(R.id.pv_main); // creating player view
+// Log.d("ADSSS",""+ getPubmaticPublisherId(baseActivity)+"=="+getPubmaticProfileId(baseActivity)+"=="+
+//                homeBannerAdsModel.getAdsid()+"=="+eventHandler );
 
-        TrackSelector trackSelectorDef = new DefaultTrackSelector();
-        // absPlayerInternal = ExoPlayerFactory.newSimpleInstance(this, trackSelectorDef); //creating a player instance
-        absPlayerInternal = (new SimpleExoPlayer.Builder(context))
-                .setTrackSelector(trackSelectorDef).build();
+        layout.addView(banner);
 
-        String userAgent = Util.getUserAgent(context, "sdfvf");
-        DefaultDataSourceFactory defdataSourceFactory = new DefaultDataSourceFactory(context, userAgent);
-        Uri uriOfContentUrl = Uri.parse(CONTENT_URL);
-        MediaSource mediaSource = new ProgressiveMediaSource.Factory(defdataSourceFactory).createMediaSource(uriOfContentUrl);  // creating a media source
+        // Call loadAd() on POBBannerView instance
+        banner.loadAd();
+        banner.setListener(new POBBannerViewListener(layout));
+    }
 
-        absPlayerInternal.prepare(mediaSource);
-        absPlayerInternal.setPlayWhenReady(true); // start loading video and play it at the moment a chunk of it is available offline
+    static class POBBannerViewListener extends POBBannerView.POBBannerViewListener {
+        private final String TAG = "POBBannerViewListener";
 
-        playerView.setPlayer(absPlayerInternal); // attach surface to the view
+        LinearLayout adslayout;
 
+        //ZeeNewsTextView advertisementText;
+        POBBannerViewListener(LinearLayout adslayout) {
+            this.adslayout = adslayout;
+            ///   this.advertisementText = advertisementText;
+        }
 
-/*//Taboola Start
-        TaboolaWidget
-                taboolaWidget = new TaboolaWidget(context);
-        int height = SdkDetailsHelper.getDisplayHeight(taboolaWidget.getContext()) * 2;
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
-        taboolaWidget.setLayoutParams(params);
-taboolaWidget.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT));
-       // frameLayout.addView(taboolaWidget);
-        layout.addView(taboolaWidget);
+        // Callback method Notifies that an ad has been successfully loaded and rendered.
+        @Override
+        public void onAdReceived(POBBannerView view) {
+            Log.d(TAG, "Ad Received");
+            try {
+                if (adslayout != null) {
+                    adslayout.removeAllViews();
+                    adslayout.addView(view);
+                    adslayout.setVisibility(View.VISIBLE);
+                }
 
-        taboolaWidget.setPublisher("zeemedia-zeenewsenglishandroid")
-                .setMode("thumbs-feed-01")
-                .setPlacement("Below Article Thumbnails")
-                .setPageUrl("https://zeenews.india.com/other-sports/neeraj-chopra-gained-close-to-12-kgs-after-olympics-gold-says-he-has-lost-5-kgs-in-last-three-weeks-2423883.html")
-                .setPageType("article")
-                .setTargetType("mix");
-//fetch the content
-        taboolaWidget.fetchContent();
-        Log.d("testt", " " + taboolaWidget.setPublisher("zeemedia-zeenewsenglishandroid"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-       // Taboola End
-    */
+        }
 
-        //  getPubmatics(context);
+        // Callback method Notifies an error encountered while loading or rendering an ad.
+        @Override
+        public void onAdFailed(POBBannerView view, POBError errorCode) {
+            Log.e(TAG, errorCode.toString());
+            if (adslayout != null) {
+                adslayout.removeAllViews();
+                adslayout.setVisibility(View.GONE);
+                if (AdRequest.ERROR_CODE_INTERNAL_ERROR == errorCode.getErrorCode()) {
+                    Log.e("errorCode", "ERROR_CODE_INTERNAL_ERROR : " + errorCode.getErrorCode());
+                } else if (AdRequest.ERROR_CODE_INVALID_REQUEST == errorCode.getErrorCode()) {
+                    Log.e("errorCode", "ERROR_CODE_INVALID_REQUEST : " + errorCode.getErrorCode());
+                } else if (AdRequest.ERROR_CODE_NETWORK_ERROR == errorCode.getErrorCode()) {
+                    Log.e("errorCode", "ERROR_CODE_NETWORK_ERROR : " + errorCode.getErrorCode());
+                } else if (AdRequest.ERROR_CODE_NO_FILL == errorCode.getErrorCode()) {
+                    Log.e("errorCode", "ERROR_CODE_NO_FILL : " + errorCode.getErrorCode());
+                }
+                //Log.d("bannerAds", "onAdFailedToLoad" + "|nanoTime:-" + nanoTime + "");
+            }
+        }
 
+        // Callback method Notifies whenever current app goes in the background due to user click
+        @Override
+        public void onAppLeaving(POBBannerView view) {
+            Log.d(TAG, "App Leaving");
+        }
 
-        //  getYoubora(context);
+        // Callback method Notifies that the banner ad view will launch a dialog on top of the current view
+        @Override
+        public void onAdOpened(POBBannerView view) {
+            Log.d(TAG, "Ad Opened");
+        }
+
+        // Callback method Notifies that the banner ad view has dismissed the modal on top of the current view
+        @Override
+        public void onAdClosed(POBBannerView view) {
+            Log.d(TAG, "Ad Closed");
+        }
 
     }
 
     @Override
     public View getView() {
-        return lin_lay;
+        return layout;
     }
 
     @Override
     public void dispose() {
     }
-
-
 }
