05fa93c6b24c0ce8853906a63e0cfa4346feacaf
logicgaurav <logicgaurav@gmail.com>
2022-01-26 22:43:37 +0530

    Little bit

--- android/app/src/main/java/com/example/flutter_native_android_work/NativeView.java
+++ android/app/src/main/java/com/example/flutter_native_android_work/NativeView.java
@@ -1,110 +1,124 @@
 package com.example.flutter_native_android_work;
 
+
 import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
-import com.example.flutter_native_android_work.R;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
+import com.npaw.youbora.lib6.exoplayer2.BuildConfig;
 import com.npaw.youbora.lib6.exoplayer2.Exoplayer2Adapter;
 import com.npaw.youbora.lib6.plugin.Options;
 import com.npaw.youbora.lib6.plugin.Plugin;
-//import com.pubmatic.sdk.common.POBError;
-//import com.pubmatic.sdk.openwrap.banner.POBBannerView;
-import com.pubmatic.sdk.openwrap.eventhandler.dfp.DFPBannerEventHandler;
 
 import java.util.Map;
 
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugin.platform.PlatformView;
 
 class NativeView implements PlatformView {
-    private final LinearLayout layout;
     LinearLayout lin_lay;
-    // private SimpleExoPlayer player;
-    // private DefaultTrackSelector trackSelector;
-    SimpleExoPlayer absPlayerInternal;
-    // PlayerView pvMain;
+    String CONTENT_URL = "https://www.rmp-streaming.com/media/big-buck-bunny-360p.mp4";
+    private MappingTrackSelector.MappedTrackInfo mappedTrackInfo;
+    DefaultTrackSelector trackSelectorDef;
+    SimpleExoPlayer player;
+    PlayerView playerView;
 
     NativeView(Context context, int id, Map<String, Object> creationParams) {
-        layout = new LinearLayout(context);
-        // textView.setTextSize(42);
-        layout.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT));
-        // layout.setOrientation(FrameLayout.VERTICAL);
+
         LayoutInflater inflater = LayoutInflater.from(context);
         View v = inflater.inflate(R.layout.activity_mainn, null);
-        PlayerView playerView = v.findViewById(R.id.player_view);
+        playerView = v.findViewById(R.id.player_view);
         lin_lay = v.findViewById(R.id.lin_lay);
 
         lin_lay.setBackgroundColor(context.getResources().getColor(R.color.browser_actions_divider_color));
 
-        String CONTENT_URL = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
-
-        int appNameStringRes = R.string.app_name;
-
-        // pvMain = findViewById(R.id.pv_main); // creating player view
-
-        TrackSelector trackSelectorDef = new DefaultTrackSelector();
-        // absPlayerInternal = ExoPlayerFactory.newSimpleInstance(this, trackSelectorDef); //creating a player instance
-        absPlayerInternal = (new SimpleExoPlayer.Builder(context))
-                .setTrackSelector(trackSelectorDef).build();
-
-        String userAgent = Util.getUserAgent(context, "sdfvf");
-        DefaultDataSourceFactory defdataSourceFactory = new DefaultDataSourceFactory(context, userAgent);
-        Uri uriOfContentUrl = Uri.parse(CONTENT_URL);
-        MediaSource mediaSource = new ProgressiveMediaSource.Factory(defdataSourceFactory).createMediaSource(uriOfContentUrl);  // creating a media source
-
-        absPlayerInternal.prepare(mediaSource);
-        absPlayerInternal.setPlayWhenReady(true); // start loading video and play it at the moment a chunk of it is available offline
-
-        playerView.setPlayer(absPlayerInternal); // attach surface to the view
-
-
-/*//Taboola Start
-        TaboolaWidget
-                taboolaWidget = new TaboolaWidget(context);
-        int height = SdkDetailsHelper.getDisplayHeight(taboolaWidget.getContext()) * 2;
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
-        taboolaWidget.setLayoutParams(params);
-taboolaWidget.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT));
-       // frameLayout.addView(taboolaWidget);
-        layout.addView(taboolaWidget);
-
-        taboolaWidget.setPublisher("zeemedia-zeenewsenglishandroid")
-                .setMode("thumbs-feed-01")
-                .setPlacement("Below Article Thumbnails")
-                .setPageUrl("https://zeenews.india.com/other-sports/neeraj-chopra-gained-close-to-12-kgs-after-olympics-gold-says-he-has-lost-5-kgs-in-last-three-weeks-2423883.html")
-                .setPageType("article")
-                .setTargetType("mix");
-//fetch the content
-        taboolaWidget.fetchContent();
-        Log.d("testt", " " + taboolaWidget.setPublisher("zeemedia-zeenewsenglishandroid"));
-
-       // Taboola End
-    */
-
-        //  getPubmatics(context);
-
-
-        //  getYoubora(context);
+        trackSelectorDef = new DefaultTrackSelector();
+        processQualityTracks(context);
 
     }
+
+    void processQualityTracks(Context context) {
+     //   try {
+            mappedTrackInfo = trackSelectorDef.getCurrentMappedTrackInfo();
+            player = (new SimpleExoPlayer.Builder(context))
+                    .setTrackSelector(trackSelectorDef).build();
+            DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context,
+                    Util.getUserAgent(context, "Application Name"), bandwidthMeter);
+
+            Uri uriOfContentUrl = Uri.parse(CONTENT_URL);
+
+            MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).
+                    createMediaSource(uriOfContentUrl);  // creating a media source
+
+            player.prepare(mediaSource);
+            player.setPlayWhenReady(true); // start loading video and play it at the moment a chunk of it is available offline
+
+            playerView.setPlayer(player); // attach surface to the view
+
+            getYoubora(context);
+
+       /* } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("jjhjhj",""+e.getMessage());
+        }*/
+    }
+
+
+    private void getYoubora(Context ctx) {
+      //   if (player != null && ctx instanceof MainActivity) {
+       // try{
+            Options youboraOptions = new Options();
+            youboraOptions.setAccountCode("zeenewsdev");
+            Plugin youboraPlugin = new Plugin(youboraOptions, ctx);
+            setOptions(youboraOptions, ctx);
+            youboraPlugin.setActivity((MainActivity) ctx);
+            Exoplayer2Adapter adapter = new Exoplayer2Adapter(player);
+            youboraPlugin.setAdapter(adapter);
+            adapter.setCustomEventLogger(trackSelectorDef);
+       /* }catch (Exception e) {
+            e.printStackTrace();
+            Log.d("jjhjhj",""+e.getMessage());
+        }*/
+
+    }
+   //   }
+
+    private void setOptions(Options youboraOptions, Context context) {
+        //  if (cModel != null) {
+        youboraOptions.setContentResource(CONTENT_URL);
+        youboraOptions.setContentCustomDimension1("Zee News");
+        youboraOptions.setContentCustomDimension2("Zee News" + " App");
+        youboraOptions.setContentCustomDimension3("Test");
+        youboraOptions.setContentCustomDimension4("Test");
+        youboraOptions.setContentCustomDimension6("Test");
+        youboraOptions.setContentCustomDimension7("Test");
+        youboraOptions.setAppName("Zee News");
+        youboraOptions.setContentId("Test");
+        youboraOptions.setContentTitle("Test");
+        youboraOptions.setContentLanguage("English");
+        youboraOptions.setAppReleaseVersion(BuildConfig.VERSION_NAME);
+//            youboraOptions.setHost(cModel.getWebsiteurl());
+    }
+    //  }
 
     @Override
     public View getView() {
@@ -114,6 +128,5 @@ taboolaWidget.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutPar
     @Override
     public void dispose() {
     }
-
 
 }
